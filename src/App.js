4b7b00086581de80bfd3f2f2b3645253c82badd0
Ruihua Ryan Su <rh.ryan.su@gmail.com>
2022-07-01 03:14:46 +1000

    v1.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import ResponsiveAppBar from './components/responsiveAppBar.component';
+import CustomizedTables from './components/customizedTables.component';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+
+  useEffect(() => {
+    fetch('http://192.168.1.103:8080/sunscreen/getAll')
+      .then(res => res.json())
+      .then(data => setData(data));
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ResponsiveAppBar />
+      <span></span>
+      <CustomizedTables data={data}/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Add } from './routes/add.route';
+import { Edit } from './routes/edit.route';
+import { Delete } from './routes/delete.route';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="add" element={<Add />} />
+      <Route path="edit" element={<Edit />} />
+      <Route path="delete" element={<Delete />} />
+    </Routes>
+      
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
